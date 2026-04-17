520d7d74f0c57ad7d8696b98567399f41fa960da
Hardik <184666881+HardikKharwa@users.noreply.github.com>
2026-04-17 20:40:07 +0530

    Changes Update

---
+++ app/src/main/java/com/example/srish/newLogin.java
@@ -0,0 +1,106 @@
+package com.example.srish;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import tybca.sem6.android.DB.UsersDB;
+
+public class newLogin extends AppCompatActivity {
+    EditText _login_et_username,_login_et_password;
+    Button _login_btn_login;
+    TextView _login_tv_forgotPassword,_login_tv_newUser;
+    String username,password;
+    UsersDB usersDB;
+    public static final String myPreference = "myPrefs";
+    SharedPreferences sharedPreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_new_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        _login_et_username = findViewById(R.id.login_et_username);
+        _login_et_password = findViewById(R.id.login_et_password);
+        _login_btn_login = findViewById(R.id.login_btn_login);
+        _login_tv_forgotPassword = findViewById(R.id.login_tv_forgotPassword);
+        _login_tv_newUser = findViewById(R.id.login_tv_newUser);
+        usersDB = new UsersDB(newLogin.this);
+        sharedPreferences = getSharedPreferences(myPreference, Context.MODE_PRIVATE);
+        if(sharedPreferences.getAll().containsKey("isLoggedIn")){
+            if(sharedPreferences.getAll().containsKey("username")){
+                Intent i = new Intent(newLogin.this,SecondActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }
+
+        _login_btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                username = _login_et_username.getText().toString().trim();
+                password = _login_et_password.getText().toString().trim();
+                if(username.isEmpty()){
+                    _login_et_username.setError("Enter Username");
+                    _login_et_username.requestFocus();
+                }
+                else if(password.isEmpty()){
+                    _login_et_password.setError("Enter Password");
+                    _login_et_password.requestFocus();
+                }
+                else if(password.length() <= 8){
+                    _login_et_password.setError("Password cannot be less than 8 characters");
+                }
+                else{
+                    Cursor cursor = usersDB.getUserData(username);
+                    if(cursor.getCount() == 0){
+                        Toast.makeText(newLogin.this, "Login Failed, Try Again!", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        if(cursor.moveToFirst()){
+                            if(password.equals(cursor.getString(4))){
+                                Toast.makeText(newLogin.this, "Login Successfully", Toast.LENGTH_SHORT).show();
+                                SharedPreferences.Editor editor = sharedPreferences.edit();
+                                editor.putString("username",username);
+                                editor.putString("user_id",cursor.getString(0));
+                                editor.putString("isLoggedIn","1");
+                                editor.commit();
+                                Intent i = new Intent(newLogin.this,SecondActivity.class);
+                                startActivity(i);
+                                finish();
+                            }
+                            else{
+                                Toast.makeText(newLogin.this, "Username / Password Doesnot Match", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                }
+            }
+        });
+    }
+    public void redirectToNewRegistration(View view){
+        Intent i = new Intent(newLogin.this,newRegistration.class);
+        startActivity(i);
+    }
+}
---
+++ app/src/main/java/com/example/srish/newRegistration.java
@@ -0,0 +1,107 @@
+package com.example.srish;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+import tybca.sem6.android.DB.UsersDB;
+
+public class newRegistration extends AppCompatActivity {
+    TextInputEditText _newregistration_et_username, _newregistration_et_password, _newregistration_et_email, _newregistration_et_contact;
+    TextInputLayout _newregistration_etl_username, _newregistration_etl_password, _newregistration_etl_email, _newregistration_etl_contact;
+    Button _newregistration_btn_submit;
+    TextView _newregistration_tv_data;
+    String username, password, email, contact, data;
+    UsersDB usersDB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_new_registration);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        usersDB = new UsersDB(newRegistration.this);
+        _newregistration_et_username = findViewById(R.id.newregistration_et_username);
+        _newregistration_et_password = findViewById(R.id.newregistration_et_password);
+        _newregistration_et_email = findViewById(R.id.newregistration_et_email);
+        _newregistration_et_contact = findViewById(R.id.newregistration_et_contact);
+        _newregistration_btn_submit = findViewById(R.id.newregistration_btn_submit);
+        _newregistration_tv_data = findViewById(R.id.newregistration_tv_data);
+        _newregistration_etl_username = findViewById(R.id.newregistration_etl_username);
+        _newregistration_etl_password = findViewById(R.id.newregistration_etl_password);
+        _newregistration_etl_email = findViewById(R.id.newregistration_etl_email);
+        _newregistration_etl_contact = findViewById(R.id.newregistration_etl_contact);
+
+    }
+
+    public void NewregisterData(View view) {
+        username = _newregistration_et_username.getText().toString().trim();
+        password = _newregistration_et_password.getText().toString().trim();
+        email = _newregistration_et_email.getText().toString().trim();
+        contact = _newregistration_et_contact.getText().toString().trim();
+        if (username.isEmpty()) {
+            _newregistration_etl_username.setError("Username cannot be empty");
+            _newregistration_etl_username.requestFocus();
+            return;
+        } else {
+            _newregistration_etl_username.setErrorEnabled(false);
+        }
+        if (password.isEmpty()) {
+            _newregistration_etl_password.setError("Password cannot be empty");
+            _newregistration_etl_password.requestFocus();
+            return;
+        } else {
+            _newregistration_etl_password.setErrorEnabled(false);
+        }
+        if (password.length() < 8) {
+            _newregistration_etl_password.setError("Password cannot be less than 8 digits");
+            _newregistration_etl_password.requestFocus();
+            return;
+        } else {
+            _newregistration_etl_password.setErrorEnabled(false);
+        }
+        if (email.isEmpty()) {
+            _newregistration_etl_email.setError("Email cannot be empty");
+            _newregistration_etl_email.requestFocus();
+            return;
+        } else {
+            _newregistration_etl_email.setErrorEnabled(false);
+        }
+        if (contact.isEmpty() || contact.length() != 10) {
+            _newregistration_etl_contact.setError("Contact cannot be empty or less than 10 digits");
+            _newregistration_etl_contact.requestFocus();
+            return;
+        } else {
+            _newregistration_etl_contact.setErrorEnabled(false);
+        }
+        String result = usersDB.insertUserData(username, email, contact, password);
+        if (result.equals("Error")) {
+            Toast.makeText(this, "Failed to Create User, Try Again!", Toast.LENGTH_LONG).show();
+        } else if (result.equals("Success")) {
+            Toast.makeText(this, "User Registration Successfully Done.", Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(newRegistration.this, newLogin.class);
+            startActivity(i);
+            finish();
+        } else if (result.equals("DuplicateUser")) {
+            Toast.makeText(this, "User Already Registered with this Email ID", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "Some Error Occured Try Again!", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
